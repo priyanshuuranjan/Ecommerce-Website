0588c6c00e6e90499d57272a9fd710bafa2f83bd
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-07 03:03:19 +0530

    Adding Context

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,29 +1,13 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useEffect } from "react";
 import axios from "axios";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
-// Define your reducer and initial state
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_LOADING":
-      return { ...state, loading: true };
-    case "SET_PRODUCTS":
-      return { ...state, loading: false, products: action.payload };
-    default:
-      throw new Error(`Unsupported action type ${action.type}`);
-  }
-};
-
-const initialState = { loading: false, products: [] };
-
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   const getProducts = async (url) => {
-    dispatch({ type: "SET_LOADING" });
+   
     try {
       const res = await axios.get(url);
       const products = await res.data;
@@ -31,10 +15,7 @@ const AppProvider = ({ children }) => {
         "🚀 ~ file: productcontex.js ~ line 18 ~ getProducts ~ products",
         products
       );
-      dispatch({ type: "SET_PRODUCTS", payload: products });
-    } catch (error) {
-      console.error(error);
-    }
+    } catch (error) {}
   };
 
   useEffect(() => {
@@ -42,7 +23,7 @@ const AppProvider = ({ children }) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{  }}>{children}</AppContext.Provider>
   );
 };
 
