2de9fceaf1138987fd6986e9798a7642c329cd41
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-06 23:40:21 +0530

    Adding Context

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,12 +1,20 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
+import { useProductContext } from "../../context/productContext";
 
 const About = () => {
+  const { myName } = useProductContext();
+
   const data = {
     name: " Kumar Fertilizer Ecommerce ",
   };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      {myName}
+      <HeroSection myData={data} />
+    </>
+  );
 };
 
 export default About;
