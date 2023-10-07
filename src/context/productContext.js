c84830585ef2108e150c347260098e31401b1254
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-08 03:54:57 +0530

    working on product details

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -5,12 +5,13 @@ import  reducer  from "../reducer/productReducer"
 const AppContext = createContext();
 
 const API = "https://shop-api-ec-git-master-priyanshu-ranjans-projects.vercel.app/products";
-
 const initialState = {
   isLoading: false,
   isError: false,
   products: [],
   featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
 };
 
 const AppProvider = ({ children }) => {
@@ -27,12 +28,27 @@ const AppProvider = ({ children }) => {
     }
   };
 
+  // my 2nd api call for single product
+
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+  };
+
   useEffect(() => {
     getProducts(API);
   }, []);
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
