fa8582afeb714372280bce4d3098f77a84e0bdad
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-14 02:49:04 +0530

    working on redux

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -1,23 +1,26 @@
+// in action.js ishme hm data ko add or remove ka function likhnge 
+// redux me phle action ka kaam aata hai fir reducer ka fir last me store ka jo ki main hota hai
+
 export const ADD = (item) => {
-    return {
-        type: "ADD_CART",
-        payload: item
-    }
-}
+  return {
+    type: "ADD_CART",
+    payload: item,
+  };
+};
 
 // remove iteams
 export const DLT = (id) => {
-    return {
-        type: "RMV_CART",
-        payload: id
-    }
-}
+  return {
+    type: "RMV_CART",
+    payload: id,
+  };
+};
 
 // remove individual iteam
 
 export const REMOVE = (iteam) => {
-    return {
-        type: "RMV_ONE",
-        payload: iteam
-    }
-}
+  return {
+    type: "RMV_ONE",
+    payload: iteam,
+  };
+};
