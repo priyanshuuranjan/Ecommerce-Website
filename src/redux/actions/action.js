d3adba7c16a46148ef315b279c97529dbcd920f7
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-14 02:52:33 +0530

    working on redux

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -1,6 +1,9 @@
 // in action.js ishme hm data ko add or remove ka function likhnge 
 // redux me phle action ka kaam aata hai fir reducer ka fir last me store ka jo ki main hota hai
 
+//? hmare pass bhut sare items honge to sara ka data alg alg krna hoga jaise ki agr product 1 add kiye to ushka alg same for product2
+//? or ye kaam playload krega 
+
 export const ADD = (item) => {
   return {
     type: "ADD_CART",
