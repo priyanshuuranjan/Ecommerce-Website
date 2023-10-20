a6b57ce0582e53b4aad4265b5db441d687d79a28
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-20 21:08:01 +0530

    Working on carts

--- src/cardData/Seeds/SeedData.js
+++ src/cardData/Seeds/SeedData.js
@@ -55,6 +55,62 @@ const Cardsdata = [
         "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
       qnty: 0,
     },
+    {
+        id: "groomer_dap",
+        name: "DAP",
+         imgdata: "/images/dap2.png",
+        address: "18:46:0",
+        delimg:
+          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 1425,
+        rating: "4.9",
+        arrimg:
+          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty: 0,
+      },
+      {
+        id: "mop",
+        name: "POTASH",
+        imgdata:"/images/mop.PNG",
+        address: "60% k2o",
+        delimg:
+          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 1900,
+        rating: "4.2",
+        arrimg:
+          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty: 0,
+      },
+      {
+        id: "groomer_urea",
+        name: "UREA",
+        imgdata:"/images/urea2.png",
+        address: "46% N",
+        delimg:
+          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 350,
+        rating: "4.4",
+        arrimg:
+          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty: 0,
+      },
+      {
+        id: "groomer_urea",
+        name: "UREA",
+        imgdata:"/images/urea2.png",
+        address: "46% N",
+        delimg:
+          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 350,
+        rating: "4.4",
+        arrimg:
+          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty: 0,
+      }
   ];
   
   export default Cardsdata;
