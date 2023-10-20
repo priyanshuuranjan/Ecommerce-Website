c4538f268fa9bf9e722758de5ef3fa6e35dcfbff
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-21 00:42:35 +0530

    Working on items on cart

--- src/cardData/Seeds/SeedData.js
+++ src/cardData/Seeds/SeedData.js
@@ -1,117 +1,158 @@
 const Cardsdata = [
-    {
-      id: "1",
-      name: " Bitter Gourd ",
-      imgdata: "/images/seed/s1.jpg",
-      address: "VNR NT 77",
-      delimg:
-        "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-      somedata: " 1175 + order placed from here recently",
-      price: 580,
-      rating: "4.7",
-      arrimg:
-        "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-      qnty: 0,
-    },
-    {
-      id: "2",
-      name: " TOMATO",
-       imgdata: "/images/seed/s2.jpg",
-      address: "VNR 3171",
-      delimg:
-        "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-      somedata: " 2525 + order placed from here recently",
-      price: 180,
-      rating: "4.9",
-      arrimg:
-        "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-      qnty: 0,
-    },
-    {
-      id: "3",
-      name: "BRINJAL",
-      imgdata:"/images/seed/s3jpg",
-      address: "F1 Hybrid",
-      delimg:
-        "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-      somedata: " 650 + order placed from here recently",
-      price: 490,
-      rating: "4.2",
-      arrimg:
-        "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-      qnty: 0,
-    },
-    {
-      id: "4",
-      name: "CAULIFLOWER",
-      imgdata:"/images/seed/s4.jpg",
-      address: "Clause",
-      delimg:
-        "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-      somedata: " 300 + order placed from here recently",
-      price: 350,
-      rating: "4.4",
-      arrimg:
-        "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-      qnty: 0,
-    },
-    {
-        id: "5",
-        name: "BRINJAL",
-         imgdata: "/images/seed/s5.jpg",
-        address: " F1 Hybrid",
-        delimg:
-          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-        somedata: " 2525 + order placed from here recently",
-        price: 298,
-        rating: "4.9",
-        arrimg:
-          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-        qnty: 0,
-      },
-      {
-        id: "6",
-        name: "PEA",
-        imgdata:"/images/seed/s6.jpg",
-        address: "F1 Hybrid",
-        delimg:
-          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-        somedata: " 650 + order placed from here recently",
-        price: 390,
-        rating: "4.2",
-        arrimg:
-          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-        qnty: 0,
-      },
-      {
-        id: "7",
-        name: "BRINJAL",
-        imgdata:"/images/seed/s7.jpg",
-        address: "BSS 1031",
-        delimg:
-          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-        somedata: " 300 + order placed from here recently",
-        price: 310,
-        rating: "4.4",
-        arrimg:
-          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-        qnty: 0,
-      },
-      {
-        id: "8",
-        name: "BOTTLE GAURD",
-        imgdata:"/images/seed/s8.jpg",
-        address: "F1 Hybrid",
-        delimg:
-          "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
-        somedata: " 300 + order placed from here recently",
-        price: 350,
-        rating: "4.4",
-        arrimg:
-          "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
-        qnty: 0,
-      }
-  ];
-  
-  export default Cardsdata;
-  
+  {
+    id: "1",
+    name: " Bitter Gourd ",
+    imgdata: "/images/seed/s1.jpg",
+    address: "VNR NT 77",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 1175 + order placed from here recently",
+    price: 580,
+    rating: "4.7",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "2",
+    name: " TOMATO",
+    imgdata: "/images/seed/s2.jpg",
+    address: "VNR 3171",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 2525 + order placed from here recently",
+    price: 180,
+    rating: "4.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "3",
+    name: "BRINJAL",
+    imgdata: "/images/seed/s3.jpg",
+    address: "F1 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 650 + order placed from here recently",
+    price: 490,
+    rating: "4.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "4",
+    name: "CAULIFLOWER",
+    imgdata: "/images/seed/s4.jpg",
+    address: "Clause",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 350,
+    rating: "4.4",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "5",
+    name: "BRINJAL",
+    imgdata: "/images/seed/s5.jpg",
+    address: " F1 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 2525 + order placed from here recently",
+    price: 298,
+    rating: "4.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "6",
+    name: "PEA",
+    imgdata: "/images/seed/s6.jpg",
+    address: "F1 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 650 + order placed from here recently",
+    price: 390,
+    rating: "4.6",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "7",
+    name: "BRINJAL",
+    imgdata: "/images/seed/s7.jpg",
+    address: "BSS 1031",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 1300 + order placed from here recently",
+    price: 310,
+    rating: "4.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "8",
+    name: "BOTTLE GOURD",
+    imgdata: "/images/seed/s8.jpg",
+    address: "F1 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 222 + order placed from here recently",
+    price: 350,
+    rating: "4.0",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "9",
+    name: "BITTER GOURD",
+    imgdata: "/images/seed/s9.jpg",
+    address: "SW-811",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 350,
+    rating: "4.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "10",
+    name: "BITTER GOURD",
+    imgdata: "/images/seed/s10.jpg",
+    address: "065 F1 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 850,
+    rating: "4.7",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: "11",
+    name: "BITTER GOURD",
+    imgdata: "/images/seed/s11.jpg",
+    address: "NT77 Hybrid",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.jpg?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 200,
+    rating: "4.5",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.jpg?output-format=webp",
+    qnty: 0,
+  },
+];
+
+export default Cardsdata;
