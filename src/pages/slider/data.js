85f66d9890b862768da7b8d3258373056fe97a4d
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-11-15 00:06:46 +0530

    Working on review

--- src/pages/slider/data.js
+++ src/pages/slider/data.js
@@ -1,37 +1,36 @@
 const people = [
-    {
-      id: 1,
-      image: "/images/fertilizer.jpg",
-      name: "Haryy Stamper",
-      title: "manager",
-      quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    },
-    {
-      id: 2,
-      image: "/images/Person2.png",
-      name: "James Gun",
-      title: "Project Manager",
-      quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    },
-    {
-      id: 3,
-      image: "/images/Person3.jpg",
-      name: "Michael Mosely",
-      title: "Product Manager",
-      quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 4,
-      image: "/images/Person4.jpg",
-      name: "James Van",
-      title: "Vice President",
-      quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-  ];
-  
-  export default people;
-  
+  {
+    id: 1,
+    image: "/images/fertilizer.jpg",
+    name: "Virendra Singh",
+    title: "Founder",
+    quote:
+      "As the founder of Kumar Fertilizer since 2002, I lead India's premier online agricultural supplier. Specializing in a complete range of farming essentials—seeds, fertilizers, pesticides, plant growth regulators, irrigation, and tools—our mission is rooted in providing quality at competitive prices with dedicated customer support. Kumar Fertilizer is more than a business; it's a commitment to excellence and agricultural success. Our legacy thrives on delivering reliable, high-quality products, nurturing the growth of agriculture, and empowering farmers nationwide. At Kumar Fertilizer, our journey is marked by an unwavering dedication to fostering prosperity in every field we touch. ",
+  },
+  {
+    id: 2,
+    image: "/images/Person2.png",
+    name: "Priyanshu Singh",
+    title: "Developer",
+    quote:
+      "As CEO and developer, I drive our company's tech journey, crafting innovative websites that redefine online experiences. Specializing in cutting-edge development, we offer tailored digital solutions. Leadership is more than coding; it's about shaping a digital legacy marked by creativity, functionality, and client satisfaction. Our mission is clear: to provide top-notch websites that stand out. As both developer and CEO, I ensure each project reflects our commitment to excellence and tech advancement, fostering a digital landscape where our creations make a lasting impact. ",
+  },
+  {
+    id: 3,
+    image: "/images/Person3.jpg",
+    name: "Amit Singh",
+    title: "Product Manager",
+    quote:
+    "As the Product Manager, I drive our company's path, crafting innovative products that redefine market experiences. Specializing in strategic development, we offer solutions tailored to diverse consumer needs. Leadership in product management extends beyond features; it's about creating a legacy marked by innovation, usability, and customer satisfaction. Our mission is clear: to deliver standout products that resonate in the market. As Product Manager, I ensure each project reflects our commitment to excellence, fostering a product landscape where our creations make a lasting impact on consumers and industry standards. "
+  },
+  {
+    id: 4,
+    image: "/images/Person4.jpg",
+    name: "James Van",
+    title: "Vice President",
+    quote:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+];
+
+export default people;
