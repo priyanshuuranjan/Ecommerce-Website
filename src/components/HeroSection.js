0e54b5ced21042f3553a82d63708637246407403
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-02-22 19:28:18 +0530

    Adding Hero-section images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,31 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../styles/Button";
+import { useState, useEffect } from "react";
 
 const HeroSection = ({ myData }) => {
   const { name } = myData;
+
+  const images = [
+    "https://media3.giphy.com/media/3o7btNUijQYS0WAcwg/giphy.webp?cid=790b76118pfswxxvce2v3qpehhimx6h9bkocbx9ge2z5p9nc&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+    "https://media1.giphy.com/media/l1J3qfEz3nmxWL4Vq/giphy.webp?cid=ecf05e47yvphraz43rvcxllml5ota1y67gnlgqriavwd38wf&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+    "https://media4.giphy.com/media/3oEjHOUcNRKgpqTHiM/giphy.webp?cid=ecf05e47r3sc40pj7b3097usccmzl3wmtmfa6js6twy8ld07&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+    "https://media4.giphy.com/media/De6pVg0gDX82safJ0w/giphy.webp?cid=790b76118xuyjy8xuvmeojpzo0hgaq1o6q2od7a5udnxyml1&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+    "https://www.ugaoo.com/cdn/shop/files/Subscription_Stop_Motion.gif?v=1666090774&width=750",
+    "https://media0.giphy.com/media/nzyOtzTLP8Htb7aD1R/giphy.webp?cid=ecf05e47sptq28lnf6mdwk2ttk58iiz3b7ex66lfpw2ci9j1&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+    "https://media3.giphy.com/media/IUgHetqwLuMOle17DC/giphy.webp?cid=790b76114uprl08e1dbc935kz27px9yfcffeinrq76ixzr1c&ep=v1_gifs_search&rid=giphy.webp&ct=g",
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Increment currentImageIndex, and loop back to 0 if it exceeds the length of images array
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []); // Empty dependency array to run only once on component mount
 
   return (
     <Wrapper>
@@ -27,12 +49,10 @@ const HeroSection = ({ myData }) => {
             </NavLink>
           </div>
 
-          {/* our homepage image  */}
-
           <div className="hero-section-image">
             <figure>
               <img
-                src="images/hero.jpg"
+                src={images[currentImageIndex]}
                 alt="hero-section-photo"
                 className="img-style"
               />
